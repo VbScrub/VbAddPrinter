65ffaf9100de9a82983bcfee2146cb52edf2d7ef
VbScrub <53273102+ChrisTo54@users.noreply.github.com>
2020-02-17 13:57:52 +0000

    Add files via upload

---
+++ MainModule.vb
@@ -0,0 +1,51 @@
+﻿
+Imports System.Runtime.InteropServices
+
+Module MainModule
+
+    Sub Main()
+        Try
+            If My.Application.CommandLineArgs.Count = 0 OrElse My.Application.CommandLineArgs(0) = "-h" OrElse My.Application.CommandLineArgs(0) = "/?" Then
+                Console.WriteLine("Valid arguments: " & Environment.NewLine &
+                              "VbAddPrinter.exe add [printer_name] [driver_name] [port_name]""" & Environment.NewLine &
+                              "Or" & Environment.NewLine &
+                              "VbAddPrinter.exe enum")
+                Return
+            End If
+
+            If My.Application.CommandLineArgs(0).ToLower = "add" Then
+                Dim PrinterInfo As New WinApi.PRINTER_INFO_2
+                PrinterInfo.pPrinterName = My.Application.CommandLineArgs(1)
+                PrinterInfo.pDriverName = My.Application.CommandLineArgs(2)
+                PrinterInfo.pPortName = My.Application.CommandLineArgs(3)
+                Dim printerHandle As IntPtr = WinApi.AddPrinter(Nothing, 2, PrinterInfo)
+                If Not printerHandle = IntPtr.Zero Then
+                    Console.Write("Printer successfully added")
+                    WinApi.ClosePrinter(printerHandle)
+                Else
+                    Console.WriteLine("Failed with error code " & Marshal.GetLastWin32Error())
+                End If
+            ElseIf My.Application.CommandLineArgs(0).ToLower = "enum" Then
+                Dim NumberOfItems As UInteger
+                Dim PrintersPtr As IntPtr
+                Dim StepSize As Integer = Marshal.SizeOf(GetType(WinApi.PRINTER_INFO_1))
+                Dim RequiredBufferSize As UInteger
+                WinApi.EnumPrintersW(WinApi.PRINTER_ENUM_LOCAL, Nothing, 1, PrintersPtr, 0, RequiredBufferSize, NumberOfItems)
+                PrintersPtr = Marshal.AllocHGlobal(CInt(RequiredBufferSize))
+                If WinApi.EnumPrintersW(WinApi.PRINTER_ENUM_LOCAL, Nothing, 1, PrintersPtr, RequiredBufferSize, 0, NumberOfItems) Then
+                    For i As Integer = 0 To NumberOfItems - 1
+                        Dim CurrentPrinter As WinApi.PRINTER_INFO_1 = Marshal.PtrToStructure(Of WinApi.PRINTER_INFO_1)(PrintersPtr)
+                        Console.WriteLine("Name: " & CurrentPrinter.pName)
+                        PrintersPtr = IntPtr.Add(PrintersPtr, StepSize)
+                    Next
+                    Marshal.FreeHGlobal(PrintersPtr)
+                Else
+                    Throw New ComponentModel.Win32Exception
+                End If
+            End If
+        Catch ex As Exception
+            Console.WriteLine("Error: " & ex.Message)
+        End Try
+    End Sub
+
+End Module
---
+++ WinApi.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class WinApi
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure PRINTER_INFO_2
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pServerName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pPrinterName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pShareName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pPortName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pDriverName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pComment As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pLocation As String
+        Public pDevMode As IntPtr
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pSepFile As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pPrintProcessor As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pDatatype As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pParameters As String
+        Public pSecurityDescriptor As IntPtr
+        Public Attributes As UInteger
+        Public Priority As UInteger
+        Public DefaultPriority As UInteger
+        Public StartTime As UInteger
+        Public UntilTime As UInteger
+        Public Status As UInteger
+        Public cJobs As UInteger
+        Public AveragePPM As UInteger
+    End Structure
+
+    <StructLayoutAttribute(LayoutKind.Sequential)>
+    Public Structure PRINTER_INFO_1
+        Public Flags As UInteger
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pDescription As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pName As String
+        <MarshalAs(UnmanagedType.LPWStr)>
+        Public pComment As String
+    End Structure
+
+    Public Const PRINTER_ENUM_LOCAL As Integer = 2
+
+
+    <DllImport("Winspool.drv", EntryPoint:="AddPrinterW")>
+    Public Shared Function AddPrinter(<InAttribute(), MarshalAs(UnmanagedType.LPWStr)> ByVal pName As String, ByVal Level As UInteger,
+                                      <InAttribute()> ByRef pPrinter As PRINTER_INFO_2) As System.IntPtr
+    End Function
+
+    <DllImport("Winspool.drv", EntryPoint:="ClosePrinter")>
+    Public Shared Function ClosePrinter(<InAttribute()> ByVal Handle As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    <DllImport("Winspool.drv", EntryPoint:="EnumPrintersW")>
+    Public Shared Function EnumPrintersW(ByVal Flags As UInteger, <InAttribute(), MarshalAs(UnmanagedType.LPWStr)> ByVal Name As String,
+                                         ByVal Level As UInteger, ByVal pPrinterEnum As IntPtr, ByVal cbBuf As UInteger,
+                                         <OutAttribute()> ByRef pcbNeeded As UInteger,
+                                         <OutAttribute()> ByRef pcReturned As UInteger) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+End Class
